d6dcbc52183304ca13b212fcd9bb7f2e2fd49039
Christopher <1051666563@qq.com>
2020-12-09 00:12:06 +0800

    Commit twice at a time -- add pycs so that the main part is clearer.

---
+++ Stage_Five_Python_Package/FindTag.py
@@ -0,0 +1,56 @@
+from urllib.parse import quote
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+MoeGirlLink = 'https://zh.moegirl.org.cn'
+SearchPageBase = 'https://zh.moegirl.org.cn/index.php?'
+BaiduSearchBase = 'https://baike.baidu.com/search/none?word='
+BaiduWikiBase = 'https://baike.baidu.com'
+
+
+def makeSearchedPage(search):
+    ReturnUrl = SearchPageBase + 'search=' + str(search) + '&title=Special'
+    return ReturnUrl
+
+
+def repairName(Name):
+    EncodeName = str(Name.encode())
+    print("EncodeName " + EncodeName)
+    if '\\' not in EncodeName:
+        Name = Name.replace(' ', '+')
+    else:
+        Name = quote(Name, encoding='utf-8', errors='replace')
+    return Name
+
+
+def getResult(Name):
+    name = repairName(Name)
+    url = BaiduSearchBase + name
+    html = urlopen(url)
+    bs = BeautifulSoup(html, 'lxml')
+    Link = bs.find('div', {'class': 'spell-correct'})
+    if Link:
+        return BaiduWikiBase + Link.find('a').attrs['href']
+    status = bs.find('dl', {'class': 'search-list'})
+    if status is None:
+        if len(Name) >= 1:
+            return getResult(Name[:len(Name)-1])
+        print("We have not find any Results.\nPlease Try again.")
+        return None
+    link = status.find('dd').find('a').attrs['href']
+    return link
+
+
+def findTag(url):
+    html = urlopen(url)
+    bs = BeautifulSoup(html, 'lxml')
+    Name = str(bs.find('br').nextSibling)
+    if Name is None:
+        exit("Ops! Wrong Name!")
+    Tag = Name.replace(' ', '_')
+    return Tag
+
+
+def FindPerson(name):
+    return findTag(getResult(name))
+
+print(FindPerson('御坂琴'))
